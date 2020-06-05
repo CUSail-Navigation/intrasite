31edcde1429a106cf5d6c3fc9ff21efc4d24425c
Courtney McBeth <clm325@cornell.edu>
2020-06-04 20:53:30 -0400

    try auth again

--- js/auth.js
+++ js/auth.js
@@ -1,13 +1,8 @@
 $(document).ready(function () {
   const login_url = 'https://github.com/login/oauth/authorize?client_id=aea1d3ebf253d278dee2';
-  $.ajax({
-    url: login_url,
-    type: "GET",
-    success: function (result) {
-      console.log(result)
-    },
-    error: function (error) {
-      console.log('Error ${error}')
+  $.get(
+    login_url, function (data, status) {
+      console.log('${data}')
     }
-  })
+  )
 })
