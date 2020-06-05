8bfb20befcb58465f0a81fac5e709a58eeafe709
Courtney McBeth <clm325@cornell.edu>
2020-06-05 15:35:37 -0400

    try again

--- js/auth.js
+++ js/auth.js
@@ -16,11 +16,13 @@ var temp_code = getQueryVariable('code');
 if (temp_code) {
   var access_url = 'https://cors-anywhere.herokuapp.com/';
   access_url += 'https://github.com/login/oauth/access_token';
-  access_url += '?client_id=aea1d3ebf253d278dee2';
-  access_url += '?client_secret=0f1fe723b7838ce933f8eb7d93deecaeea910dba';
-  access_url += '?code=';
-  access_url += temp_code;
-  access_url += '?redirect_uri=https://cusail-navigation.github.io/intrasite/progress2020-2021';
+  var send_data = 'client_id=aea1d3ebf253d278dee2';
+  send_data += '&client_secret=0f1fe723b7838ce933f8eb7d93deecaeea910dba';
+  send_data += '&code=';
+  send_data += temp_code;
+  // access_url += '?redirect_uri=https://cusail-navigation.github.io/intrasite/progress2020-2021';
+  console.log(temp_code);
+  console.log(access_url);
   var xhr = new XMLHttpRequest();
   xhr.open("POST", access_url, true);
   xhr.setRequestHeader('Content-Type', 'application/json');
@@ -29,7 +31,7 @@ if (temp_code) {
       console.log(this.responseText);
     }
   }
-  xhr.send();
+  xhr.send(send_data);
 } else {
   // somehow bypassed, send them back
   window.location.replace('https://github.com/login/oauth/authorize?client_id=aea1d3ebf253d278dee2&redirect_uri=https://cusail-navigation.github.io/intrasite/progress_gateway');
