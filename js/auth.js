f8e540a1d26dfbda9c149680462e95dcaa6466f9
Courtney McBeth <clm325@cornell.edu>
2020-06-05 14:55:28 -0400

    try something

--- js/auth.js
+++ js/auth.js
@@ -12,10 +12,22 @@ function getQueryVariable(variable) {
 }
 
 var temp_code = getQueryVariable('code');
-console.log(temp_code);
+var access_url = 'https://github.com/login/oauth/access_token';
+access_url += '?client_id=aea1d3ebf253d278dee2';
+access_url += '?client_secret=0f1fe723b7838ce933f8eb7d93deecaeea910dba';
+access_url += '?code=';
+access_url += temp_code;
 
 if (temp_code) {
-  // get access token
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", access_url, true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.onreadystatechange = function () { // Call a function when the state changes.
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+      console.log(this.response);
+    }
+  }
+  xhr.send();
 } else {
   // somehow bypassed, send them back
   window.location.replace('https://github.com/login/oauth/authorize?client_id=aea1d3ebf253d278dee2&redirect_uri=https://cusail-navigation.github.io/intrasite/progress2020-2021');
