d8c26a4f27295c3f367b30cac16009a40d5e10a4
Courtney McBeth <clm325@cornell.edu>
2020-06-05 17:03:49 -0400

    attempting get

--- js/progress.js
+++ js/progress.js
@@ -13,7 +13,7 @@ function getQueryVariable(variable) {
 
 function displayExistingGoals() {
   var goals = document.getElementById('goals_layout');
-  var milestone_str = ['August 2020', 'September 2020', 'October 2020', 'November 2020', 'December 2020', 'January 2020', 'February 2020', 'March 2020', 'April 2020', 'May 2020'];
+  var milestone_str = ['August 2020', 'September 2020', 'October 2020', 'November 2020', 'December 2020', 'January 2021', 'February 2021', 'March 2021', 'April 2021', 'May 2021'];
   var milestone_num = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
   var add_html = '';
@@ -21,6 +21,17 @@ function displayExistingGoals() {
   for (i = 0; i < milestone_num.length; i++) {
     add_html += '<h2>' + milestone_str[i] + '</h2>';
     // make a get request for those issues
+    var xhr = new XMLHttpRequest();
+    var get_url = 'https://api.github.com/repos/cusail-navigation/intrasite/issues';
+    get_url += '?milestone=' + milestone_num[i];
+    xhr.open('GET', get_url, true);
+    xhr.setRequestHeader('Authorization', 'token ' + getQueryVariable('auth'));
+
+    xhr.onload = function () {
+      console.log(xhr.responseText);
+    };
+
+    xhr.send();
 
     // make and display the html
   }
