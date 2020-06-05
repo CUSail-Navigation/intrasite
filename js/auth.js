710c5bed029e9ca6abe6ba884f342ea35e6adb12
Courtney McBeth <clm325@cornell.edu>
2020-06-05 13:16:40 -0400

    try proxy

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  const login_url = 'https://github.com/login/oauth/authorize?client_id=aea1d3ebf253d278dee2';
+  const login_url = 'https://cors-anywhere.herokuapp.com/https://github.com/login/oauth/authorize?client_id=aea1d3ebf253d278dee2';
   $.ajax({
     // url: 'https://api.github.com/repos/cusail-navigation/intrasite/issues',
     url: login_url,
