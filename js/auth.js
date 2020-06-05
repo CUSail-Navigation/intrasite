538b46ed5866427f35fcbc67d3586b070d90ab3b
Courtney McBeth <clm325@cornell.edu>
2020-06-05 13:07:04 -0400

    try again

--- js/auth.js
+++ js/auth.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
   const login_url = 'https://github.com/login/oauth/authorize?client_id=aea1d3ebf253d278dee2';
   $.ajax({
-    url: 'https://api.github.com/repos/cusail-navigation/intrasite/issues',
+    // url: 'https://api.github.com/repos/cusail-navigation/intrasite/issues',
+    url: login_url,
     type: "GET",
     crossDomain: true,
     success: function (response) {
