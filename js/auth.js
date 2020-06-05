9e3dd44ae35ea98c432b6830a15359ebbfc7eb9f
Courtney McBeth <clm325@cornell.edu>
2020-06-04 21:05:58 -0400

    trying hack

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  const login_url = 'https://github.com/login/oauth/authorize?client_id=aea1d3ebf253d278dee2';
+  const login_url = 'https://cors.io/?https://github.com/login/oauth/authorize?client_id=aea1d3ebf253d278dee2';
   $.get(
     login_url, function (data, status) {
       console.log('${data}')
