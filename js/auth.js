d515199524188e3cc95436d6a8568f6afa83bb05
Courtney McBeth <clm325@cornell.edu>
2020-06-05 13:40:36 -0400

    progress

--- js/auth.js
+++ js/auth.js
@@ -1,12 +1,11 @@
 $(document).ready(function () {
-  const login_url = 'https://cors-anywhere.herokuapp.com/https://github.com/login/oauth/authorize?client_id=aea1d3ebf253d278dee2';
+  const login_url = 'https://cors-anywhere.herokuapp.com/https://github.com/login/oauth/authorize?client_id=aea1d3ebf253d278dee2&redirect_uri=https://cusail-navigation.github.io/intrasite/progress2020-2021';
   $.ajax({
-    // url: 'https://api.github.com/repos/cusail-navigation/intrasite/issues',
     url: login_url,
     type: "GET",
     crossDomain: true,
     success: function (response) {
-      console.log(response)
+      document.write(response)
     },
     error: function (xhr, status) {
       alert("error");
