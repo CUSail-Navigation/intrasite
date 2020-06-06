ed90eab1ed31931b3e93066b40cc6b7a226198ad
Courtney McBeth <clm325@cornell.edu>
2020-06-05 20:18:59 -0400

    formatting

--- js/progress.js
+++ js/progress.js
@@ -11,6 +11,14 @@ function getQueryVariable(variable) {
   }
 }
 
+function parseDate(date_str) {
+  // input is something like "2020-06-05T19:13:40Z"
+  var year = date_str.substring(0, 4);
+  var month = date_str.substring(5, 7);
+  var date = date_str.substring(8, 10);
+  return date + "/" + month + "/" + year;
+}
+
 function displayExistingGoals() {
   var goals = document.getElementById('goals_layout');
   var milestone_str = ['August 2020', 'September 2020', 'October 2020', 'November 2020', 'December 2020', 'January 2021', 'February 2021', 'March 2021', 'April 2021', 'May 2021'];
@@ -35,14 +43,18 @@ function displayExistingGoals() {
       var j;
       for (j = 0; j < ret_data.length; j++) {
         add_html += '<li>';
-        add_html += '<div id="goal_creator">';
+
+        add_html += '<div id="goal_top">';
         add_html += '<img src="' + ret_data[j].user.avatar_url + '" />';
-        add_html += '<h3>' + ret_data[j].user.login + '</h3>';
-        add_html += '</div>';
-        add_html += '<h3 id="goal_title">' + ret_data[j].title + '</h3>';
+
+        add_html += '<div id="goal_creator">';
+        add_html += '<h3>' + ret_data[j].title + '</h3>';
+        add_html += '<h4>Created by ' + ret_data[j].user.login + ' on ' + parseDate(ret_data[j].created_at) + '</h4>';
+        add_html += '</div></div>';
+
 
         add_html += '<ul id="goal_assignees">';
-        add_html += '<h4>Team Members:</h4>';
+        add_html += '<h4>Assigned Team Members:</h4>';
         var k;
         for (k = 0; k < ret_data[j].assignees.length; k++) {
           add_html += '<li>' + ret_data[j].assignees[k].login + '</li>';
