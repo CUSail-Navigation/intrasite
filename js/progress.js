e7f15a5d6bb4c4dfe3d521f8dbd2036dc55b46d3
Courtney McBeth <clm325@cornell.edu>
2020-06-08 19:38:25 -0400

    change path

--- js/progress.js
+++ js/progress.js
@@ -362,7 +362,7 @@ function resetBar() {
   }
 
   // rerender the bar
-  var add_html = '<div id="main_bar" class="ldBar label-center" data-type="fill" data-img="{{site.baseurl}}/images/BoatLogo.svg" ';
+  var add_html = '<div id="main_bar" class="ldBar label-center" data-type="fill" data-img="./images/BoatLogo.svg" ';
   add_html += 'data-value="' + val + '" data-img-size="110,110" data-fill-background="#e7e7e7"></div>';
   document.getElementById('left_sidebar').innerHTML = add_html;
 }
